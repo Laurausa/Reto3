cfdb4e9487bfc3d5bf384a21774a3dea0c5e40d7
laurausa <laura.melo1.mt@correo.usa.edu.co>
2021-12-13 07:53:27 -0500

    first commit

---
+++ src/main/java/Reto2_web/interfaces/InterfaceOrder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reto2_web.interfaces;
+
+import Reto2_web.modelo.Order;
+import Reto2_web.modelo.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author laura
+ */
+public interface InterfaceOrder extends MongoRepository<Order, Integer>{
+    List<Order> findBySalesManZone(String zone);
+    
+}
---
+++ src/main/java/Reto2_web/repositorio/OrderRepositorio.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reto2_web.repositorio;
+
+import Reto2_web.interfaces.InterfaceOrder;
+import Reto2_web.modelo.Order;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author laura
+ */
+@Repository
+public class OrderRepositorio {
+    @Autowired
+    private InterfaceOrder orderCrudRepository;
+
+    public List<Order> getAll(){
+        return orderCrudRepository.findAll();
+    }
+
+    public Optional<Order> getOrder(Integer id){
+        return orderCrudRepository.findById(id);
+    }
+
+    public Order create(Order order){
+        return orderCrudRepository.save(order);
+    }
+
+    public void update(Order order){
+        orderCrudRepository.save(order);
+    }
+
+    public void delete(Order order){
+        orderCrudRepository.delete(order);
+    }
+    
+    public List<Order> getOrderByZone(String zone){
+        return orderCrudRepository.findBySalesManZone(zone);
+    }
+}
